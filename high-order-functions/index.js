726ac96bf64a4ee0341b47cf979b25c4d18310bc
Eduardo Guereque <edwardgnt@gmail.com>
2021-09-27 14:00:08 -0700

    first commit

---
+++ high-order-functions/index.js
@@ -0,0 +1,15 @@
+const companies = [
+    { name: "Company One", category: "Finance", start: 1981, end: 2003 },
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+    { name: "Company Three", category: "Auto", start: 1999, end: 2006 },
+    { name: "Company Four", category: "Retail", start: 1971, end: 2005 },
+    { name: "Company Five", category: "Technology", start: 1991, end: 2002 },
+    { name: "Company Six", category: "Finance", start: 1995, end: 2003 },
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+    { name: "Company Eight", category: "Technology", start: 1981, end: 2003 },
+    { name: "Company Nine", category: "Retail", start: 1981, end: 2003 }
+
+]
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
